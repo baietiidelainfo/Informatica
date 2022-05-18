293843d7c62b1a4bc2d36efa4953231aafe87e64
BuildTools <unconfigured@null.spigotmc.org>
2022-05-18 23:20:26 +0300

    hadir3

---
+++ Matei/atestatSir.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ofstream out("mat.out");
+    int n;
+    cin>>n;
+    int m[40][40];
+    for(int i = 0; i < n; i++){
+        for(int j = n-1; j >= 0; j--)
+                m[i][j] = (j+i%2*2)%3;
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++)
+            cout<<m[i][j]<<' ';
+        cout<<endl;
+    }
+}
